061fb28c50842406945d6b6c4894d9f57e60f580
Novakq <filipnowakowskimail@gmail.com>
2021-09-18 18:59:21 +0000

    added car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 # from .models import related models
 
+from .models import *
+
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 
 # CarModelAdmin class
 
+class CarModelAdminInline(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [CarModelInline]
+
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    
+    inlines = [CarModelInline]
+
 
 # Register models here
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,6 +3,12 @@ from django.utils.timezone import now
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -19,6 +25,24 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel (models.Model):
+    name = models.CharField(max_length=30)
+    dealer_id = models.CharField(max_length=100)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    
+    CAR_TYPES =  (
+        ('S', 'Sedan'),
+        ('M', 'SUV'),
+        ('L', 'Wagon'),
+    )
+    car_type = models.CharField(max_length=1, choices=CAR_TYPES)
+    year = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
